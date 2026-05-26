bfe4a8d103556ec6b22ce90a36a4cf5265b00b48
BluWith10Us <167274584+BluWith10Us@users.noreply.github.com>
2026-05-26 13:03:55 +0800

    Jump Added

--- Assets/Scripts/NetworkPlayerController.cs
+++ Assets/Scripts/NetworkPlayerController.cs
@@ -7,11 +7,13 @@ public class NetworkPlayerController : NetworkBehaviour
     [SerializeField] float gravity = -9.8f;
     [SerializeField] float groundedGravity = -2.2f;
     [SerializeField] float verticalVelocity = 0f;
+    [SerializeField] float jumpHeight = 5f;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
 
     public CharacterController controller;
     void Awake()
     {
-        controller.GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -21,38 +23,55 @@ public class NetworkPlayerController : NetworkBehaviour
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
         Vector2 inputDirection = new Vector2(horizontalInput, verticalInput);
 
-        if(IsServer)
+        bool jumpPressed = Input.GetKeyDown(jumpKey);
+
+        if (IsServer)
         {
-            MovePlayer(inputDirection);
+            MovePlayer(inputDirection, jumpPressed);
         }
         else
         {
-            MovePlayerRPC(inputDirection);
+            MovePlayerRPC(inputDirection, jumpPressed);
         }
     }
-    
+
     [Rpc(SendTo.Server)] //Marks the next method as an RPC that runs on the server
-    private void MovePlayerRPC(Vector2 movementInput)
+    private void MovePlayerRPC(Vector2 movementInput, bool jumpPressed)
     {
-        MovePlayer(movementInput);
+        MovePlayer(movementInput, jumpPressed);
     }
 
-    private void MovePlayer(Vector2 movemenInput)
+    private void MovePlayer(Vector2 movementInput, bool jumpPressed)
     {
-        if (controller.isGrounded && verticalVelocity < 0f)
+        if (controller.isGrounded)
         {
-            verticalVelocity = groundedGravity;
+            if (verticalVelocity < 0f)
+            {
+                verticalVelocity = groundedGravity;
+            }
+
+            if (jumpPressed)
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
         }
         else
         {
             verticalVelocity += gravity * Time.deltaTime;
         }
 
-        Vector3 moveDir = new Vector3(movemenInput.x, 0f, movemenInput.y).normalized;
+        Vector3 moveDir = new Vector3(
+            movementInput.x,
+            0f,
+            movementInput.y
+        ).normalized;
+
         Vector3 horizontalMovement = moveDir * moveSpeed;
         Vector3 verticalMovement = Vector3.up * verticalVelocity;
+
         Vector3 finalMovement = horizontalMovement + verticalMovement;
 
         controller.Move(finalMovement * Time.deltaTime);
